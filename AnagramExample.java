20476896483886eaf635e85947e14f77b1720a85
lakshmisangala382-sketch <lakshmisangala382@gmail.com>
2026-08-08 23:04:54 +0530

    Add files via upload

---
+++ AnagramExample.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s1 = "listen";
+        String s2 = "silent";
+
+        char[] ch1 = s1.toCharArray();
+        char[] ch2 = s2.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        if (Arrays.equals(ch1, ch2)) {
+            System.out.println("Anagram");
+        } else {
+            System.out.println("Not Anagram");
+        }
+    }
+}
