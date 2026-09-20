cc37d2c27d5abfffa0f3dcf994d21b768c8eafbe
lakshmisangala382-sketch <lakshmisangala382@gmail.com>
2026-09-20 22:24:16 +0530

    Update Finally.java

--- Finally.java
+++ Finally.java
@@ -1,15 +1,1 @@
-public class FinallyExample {
-    public static void main(String[] args) {
 
-        try {
-            int result = 10 / 2;
-            System.out.println(result);
-        }
-        catch (ArithmeticException e) {
-            System.out.println("Exception occurred");
-        }
-        finally {
-            System.out.println("Finally block executed");
-        }
-    }
-}
